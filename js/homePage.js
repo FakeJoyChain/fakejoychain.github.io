aa934601d11b841440c1268cec13ef2b1f537c9a
534645359@qq.com <qwertyuiop~!@123>
2021-02-04 14:52:55 +0800

    add new functions

--- js/homePage.js
+++ js/homePage.js
@@ -1,4 +1,8 @@
 import("./jquery-3.5.1.min.js");
+import("../css/layui.all.js");
+import("../css/layui.js")
+
+var layer = layui.layer;
 
 $("#stop-start").on("click", function () {
     dom = $("#stop-start");
@@ -9,4 +13,12 @@ $("#stop-start").on("click", function () {
         dom.attr("class", "layui-icon layui-icon-pause");
         document.getElementById("bgmusic").play();
     }
+})
+
+$("#top").on("click", function () {
+    window.scrollTo(0, 0);
+})
+
+$("#top").hover(function () {
+    layer.message("heheh");
 })
